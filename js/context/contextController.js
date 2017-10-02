d7e5a53e41908bf97241b22dca6512a058026c82
seeker105 <karavol27@gmail.com>
2017-10-02 15:58:56 -0500

    Errors corrected in app.js

--- js/context/contextController.js
+++ js/context/contextController.js
@@ -1,6 +1,9 @@
-angular.module('twitterApp').controller('contextController', [function(){
+angular.module('twitterApp').controller('contextController', ['$http', function($http){
     
     this.context = resolvedContext.data
 
+    this.getContext= (id) => {
+        return $http.get('tweets/' + id + '/context')
+    }
 
 }])
--- js/feed/feedService.js
+++ js/feed/feedService.js
@@ -1,5 +1,6 @@
 angular.module('twitterApp').service('feedService', ['$http', function($http){
 
+    this.feed = this.resolvedTweetFeed.data;
 
     this.getFeed = (username) => {
         return $http.get('users/@' + username + '/feed')
