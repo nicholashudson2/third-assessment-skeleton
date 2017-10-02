b42ae6ba444e302fb3218da59b3fcb75dcc32844
nicholashudson2 <nicholas.hudson2@yahoo.com>
2017-10-02 18:11:31 -0500

    Endpoints implemented for create tweet/get all tweets, updated Tweet controller, service, and component to reflect.

--- js/tweet/tweetComponent.js
+++ js/tweet/tweetComponent.js
@@ -1,4 +1,8 @@
 angular.module('twitterApp').component('tweetComponent', {
     templateUrl: "/js/tweet/tweetTemplate.html",
-   // controller: "/js/tweet/tweetController"
+    controller: "/js/tweet/tweetController",
+    bindings: {
+        resolvedTweets: '='
+    }
+    
 })
