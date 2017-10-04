d89da8ad897893381729c830745762499ffc3942
seeker105 <karavol27@gmail.com>
2017-10-03 20:49:35 -0500

    Minor correction in usernameListService

--- js/usernameList/usernameListService.js
+++ js/usernameList/usernameListService.js
@@ -1,10 +1,7 @@
 angular.module('twitterApp').service('usernameListService', function(){
 
-    this.search = (searchString) => {
-        if (searchString.charAt(0).equals('@')){
-            searchString = searchString.slice(1)
-        }
-        return $http.get('http://localhost:8090/users/@' + searchString)
+    this.getUsers = () => {
+        return $http.get('http://localhost:8090/users')
     }
     
 })
